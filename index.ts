920e398bd4a6e885c82583e5dca3387ecbfaedd9
TriNgoc <caotringoc11@gmail.com>
2025-02-16 23:33:59 +0700

    add pug

--- index.ts
+++ index.ts
@@ -4,8 +4,11 @@ dotenv.config();
 const app: Express = express();
 const port: string | number = process.env.PORT || 3001;
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('okk');
+    res.render('client/pages/tours/index');
 });
 
 app.listen(port, () => {
