910349c65757f6e66ab5780ee243678cafde180a
TriNgoc <caotringoc11@gmail.com>
2025-02-17 17:11:31 +0700

    logic: add tour to cart

--- public/js/script.js
+++ public/js/script.js
@@ -30,3 +30,35 @@ var imagesMain = new Swiper(".imagesMain", {
     swiper: imagesThumb,
   },
 });
+
+
+//cart
+const cart = localStorage.getItem('cart');
+if (!cart) {
+  localStorage.setItem('cart', JSON.stringify([]));
+}
+
+
+//add to cart
+const formAddToCart = document.querySelector('[form-add-to-cart]');
+if (formAddToCart) {
+  formAddToCart.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const quantity = parseInt(e.target.elements.quantity.value);
+    const tourId = parseInt(formAddToCart.getAttribute('tour-id'));
+    if (quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem('cart'));
+      const indexExistTour = cart.findIndex(item => item.tourId == tourId);
+      if (indexExistTour == -1) {
+        cart.push({
+          tourId: tourId,
+          quantity: quantity
+        });
+      } else {
+        cart[indexExistTour].quantity = cart[indexExistTour].quantity + quantity;
+      }
+
+      localStorage.setItem('cart', JSON.stringify(cart));
+    }
+  });
+}
