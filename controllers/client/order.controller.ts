94cc24142ca64ba180da536474159589c12453b1
TriNgoc <caotringoc11@gmail.com>
2025-02-17 22:45:34 +0700

    save data to order table

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,8 +1,32 @@
 import { Request, Response } from "express";
+import Order from "../../model/order.model";
+import { generateOrderCode } from "../../helpers/generate.helper";
 
 export const index = async (req: Request, res: Response) => {
+    const info = req.body.info;
+    const cart = req.body.cart;
 
-  console.log(req.body);
-  
-  res.json({});
+    const dataOrder = {
+        code: '',
+        fullName: info.fullName,
+        phone: info.phone,
+        note: info.note,
+        status: 'initial'
+    };
+    const order = await Order.create(dataOrder);
+    const orderId = order.dataValues.id;
+
+    const code = generateOrderCode(orderId);
+    await Order.update({
+        code: code 
+    }, {
+        where: {
+            id: orderId
+        }
+    });
+    res.json({
+        code: 'success',
+        message: 'Order successfully !',
+        orderCode: code
+    });
 };
