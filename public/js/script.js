abae652dc7d8868a0ce2cf823faa773ebf2145c4
TriNgoc <caotringoc11@gmail.com>
2025-02-17 22:33:33 +0700

    send info order to server

--- public/js/script.js
+++ public/js/script.js
@@ -170,3 +170,30 @@ const drawCart = () => {
   }
 }
 drawCart();
+
+// Book
+const formOrder = document.querySelector("[form-order]");
+if(formOrder) {
+  formOrder.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const dataFinal = {
+      info: {
+        fullName: event.target.fullName.value,
+        phone: event.target.phone.value,
+        note: event.target.note.value
+      },
+      cart: JSON.parse(localStorage.getItem("cart"))
+    };
+    fetch("/order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(dataFinal)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
+  })
+}
--- routers/client/index.router.ts
+++ routers/client/index.router.ts
@@ -2,10 +2,12 @@ import { Express } from "express";
 import { tourRouters } from "./tour.router";
 import { categoryRouters } from "./category.router";
 import { cartRouters } from "./cart.router";
+import { orderRouters } from "./order.router";
 const clientRouters = (app: Express): void => {
     app.use('/tours', tourRouters);
     app.use('/categories', categoryRouters);
     app.use('/cart', cartRouters);
+    app.use('/order', orderRouters);
 }
 
 export default clientRouters;
