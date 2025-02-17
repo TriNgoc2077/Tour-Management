10969ae2d84d6b0c9fd31d587650f720bb9732b4
TriNgoc <caotringoc11@gmail.com>
2025-02-17 22:52:12 +0700

    add to order-item table

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,7 +1,29 @@
 import { Request, Response } from "express";
 import Order from "../../model/order.model";
 import { generateOrderCode } from "../../helpers/generate.helper";
-
+import Tour from "../../model/tour.model";
+import OrderItem from "../../model/order-item.model";
+interface ITour {
+    id: number;
+    title: string;
+    code?: string;
+    images?: string;
+    image: string;
+    price?: number;
+    discount?: number;
+    price_special?: number | string;
+    information?: string;
+    schedule?: string;
+    timeStart?: Date;
+    stock?: number;
+    status?: string;
+    position?: number;
+    slug: number;
+    deleted?: boolean;
+    deletedAt?: Date;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
 export const index = async (req: Request, res: Response) => {
     const info = req.body.info;
     const cart = req.body.cart;
@@ -24,6 +46,28 @@ export const index = async (req: Request, res: Response) => {
             id: orderId
         }
     });
+
+    for (const item of cart) {
+        const dataItem = {
+            orderId: orderId,
+            tourId: item.tourId,
+            quantity: item.quantity
+        } as any;
+        const tourInfo = await Tour.findOne({
+            where: {
+                id: item.tourId,
+                deleted: false,
+                status: 'active'
+            },
+            raw: true
+        }) as unknown as ITour;
+        
+        dataItem.price = tourInfo.price;
+        dataItem.discount = tourInfo.discount;
+        dataItem.timeStart = tourInfo.timeStart;
+        await OrderItem.create(dataItem);
+    }
+
     res.json({
         code: 'success',
         message: 'Order successfully !',
