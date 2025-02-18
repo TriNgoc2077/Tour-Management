c5c2bdcb4aa1ed3c9ccc458a01657851d789764b
TriNgoc <caotringoc11@gmail.com>
2025-02-18 12:50:01 +0700

    create tour : add info tour, schedule tour

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import clientRouters from "./routers/client/index.router";
 import adminRouters from "./routers/admin/index.router";
 import moment from 'moment';
 import cors from 'cors';
+import path from "path";
 import { systemConfig } from "./config/system";
 dotenv.config();
 const app: Express = express();
@@ -15,7 +16,8 @@ app.use(cors({credentials: true}));
 //App local variables
 app.locals.moment = moment;
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
-
+//tiny mce
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
 app.use(express.static(`${__dirname}/public`));
 
 app.use(express.json());
--- routers/admin/index.router.ts
+++ routers/admin/index.router.ts
@@ -2,10 +2,12 @@ import { Express } from "express";
 import { categoryRoutes } from "./category.router";
 import { systemConfig } from "../../config/system";
 import { tourRouters } from "./tour.router";
+import { uploadRouters } from "./upload.router";
 const adminRouters = (app: Express): void => {
   const PATH_ADMIN = `/${systemConfig.prefixAdmin}`;
   
   app.use(`${PATH_ADMIN}/categories`, categoryRoutes);
   app.use(`${PATH_ADMIN}/tours`, tourRouters);
+  app.use(`${PATH_ADMIN}/upload`, uploadRouters);
 };
 export default adminRouters;
